471ed1794c4820b146cff0c53ebe7e2317dce546
Mathis <handymathis@gmail.com>
2021-03-09 22:02:25 +0100

    added API communication for login request

--- backend/src/controller/login_controller.rs
+++ backend/src/controller/login_controller.rs
@@ -1,7 +1,6 @@
-use actix_web::{web, Responder, Result};
+use actix_web::{web, Responder};
 use serde::{Serialize, Deserialize};
 use crate::database::database_service::DatabaseService;
-use actix_web::web::Json;
 
 #[derive(Deserialize)]
 pub struct LoginRequest {
@@ -11,28 +10,30 @@ pub struct LoginRequest {
 }
 
 #[derive(Serialize)]
-pub struct Response {
+struct Response {
     status: bool,
     message: String,
     token: String
 }
 
-pub async fn response(info: web::Json<LoginRequest>) -> Result<Json<Response>> {
+pub async fn response(info: web::Json<LoginRequest>) -> impl Responder {
     let db = DatabaseService::new().await;
     let status = db.login(&info.0).await;
     db.close().await;
     if status.0 {
-        Ok(Json(Response {
-            status: status.0,
-            message: "Successfully logged in".to_string(),
-            token: status.1
-        }))
+        web::HttpResponse::Ok()
+            .json(Response {
+                status: status.0,
+                message: "Successfully logged in".to_string(),
+                token: status.1
+            })
     } else {
-        Ok(Json(Response {
-            status: status.0,
-            message: "Login failed".to_string(),
-            token: "None".to_string()
-        }))
+        web::HttpResponse::Ok()
+            .json(Response {
+                status: status.0,
+                message: "Login failed".to_string(),
+                token: "None".to_string()
+            })
     }
 
 }
--- frontend-web/src/routes/login/Login.js
+++ frontend-web/src/routes/login/Login.js
@@ -1,21 +1,75 @@
 import React from 'react';
 import {Link} from "react-router-dom";
 import './Login.css';
+import cookie from 'react-cookies';
 
 export class LoginWindow extends React.Component {
+
+    state = {
+        username: "",
+        password: ""
+    };
+
+
+    handleUsername = event => {
+        this.setState((state) => {
+            return {
+                username: event.target.value,
+                password: state.password
+            };
+        });
+    };
+
+    handlePassword = event => {
+        this.setState((state) => {
+            return {
+                username: state.username,
+                password: event.target.value
+            };
+        });
+    };
+
     render() {
         return (
           <div className="card">
               <div className="card-body">
                   <h1>Login</h1>
-                  <input type="text" placeholder="username"/>
-                  <input type="text" placeholder="password"/>
-                  <button>Login</button>
+                  <input type="text" placeholder="username" onChange={this.handleUsername}/>
+                  <input type="text" placeholder="password" onChange={this.handlePassword}/>
+                  <button onClick={this.login}>Login</button>
                   <Link to="/register">
                       <h3>register</h3>
                   </Link>
               </div>
           </div>
         );
+    }
+
+    login = () => {
+
+        const json = JSON.stringify({
+            username: this.state.username,
+            password: this.state.password,
+            login_device: "web"
+        });
+
+        var xhr = new XMLHttpRequest();
+
+        xhr.open('POST', 'http://127.0.0.1:8080/login');
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.setRequestHeader('Accept', 'application/json');
+        xhr.send(json);
+
+        xhr.addEventListener('load', () => {
+            let data = JSON.parse(xhr.responseText);
+            if (data.status === true) {
+                let d = new Date();
+                d.setTime(d.getTime() + (48*3600*1000))
+                cookie.save("token", data.token, {path: "/", expires: d});
+                this.props.history.push('dashboard');
+            } else {
+                alert(data.message);
+            }
+        })
     }
 }

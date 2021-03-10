2c6ab94e1f59a3bdfe753286c1e61b3579a4df95
Mathis <handymathis@gmail.com>
2021-03-10 07:53:15 +0100

    updated login function

--- backend/src/database/actions/login.rs
+++ backend/src/database/actions/login.rs
@@ -1,23 +1,27 @@
 use sqlx::{query_as, query};
 use crate::database::database_service::DatabaseService;
 use crate::controller::login_controller::LoginRequest;
-use crate::hashing::hash;
+use pwhash::sha512_crypt::verify;
 use crate::database::models::user_model;
 use crate::token;
 
 pub async fn login(service: &DatabaseService, req: &LoginRequest) -> (bool, String) {
-    let hashed_pwd = hash(&req.password);
-    let elements: Vec<user_model::UserModel> = query_as!(user_model::UserModel, "SELECT * FROM `user_accounts` WHERE `username`=? AND `password`=? AND `status`='1';",
+
+    let elements: Vec<user_model::UserModel> = query_as!(user_model::UserModel, "SELECT * FROM `user_accounts` WHERE `username`=? AND `status`='1';",
         &req.username, hashed_pwd
     ).fetch_all(&service.conn).await.unwrap();
     if elements.len() == 1 {
-        let token = token::generate_token();
-        if req.login_device == "web" {
-            query!("UPDATE `user_accounts` SET `web_token`=? WHERE `username`=?", &token, &req.username)
-                .execute(&service.conn).await;
+        if pwhash::sha512_crypt::verify(&req.password, &elements[0].password) {
+            let token = token::generate_token();
+            if req.login_device == "web" {
+                query!("UPDATE `user_accounts` SET `web_token`=? WHERE `username`=?", &token, &req.username)
+                    .execute(&service.conn).await;
+            } else {
+                query!("UPDATE `user_accounts` SET `mobile_token`=? WHERE `username`=?", &token, &req.username)
+                    .execute(&service.conn).await;
+            }
         } else {
-            query!("UPDATE `user_accounts` SET `mobile_token`=? WHERE `username`=?", &token, &req.username)
-                .execute(&service.conn).await;
+            return (false, String::new())
         }
         return (true, token);
     } else {

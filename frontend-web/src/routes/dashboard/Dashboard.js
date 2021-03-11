9f5b909d47eef1ba6aa11e8c4da82e82eed522f9
Mathis <handymathis@gmail.com>
2021-03-11 13:30:39 +0100

    commented Dashboard.js

--- frontend-web/src/routes/dashboard/Dashboard.js
+++ frontend-web/src/routes/dashboard/Dashboard.js
@@ -3,6 +3,8 @@ import {SidebarComponent} from "../../components/Sidebar/Sidebar";
 import {checkAPICredentials} from "../../utils/CheckCreds";
 
 export class Dashboard extends React.Component {
+
+    // renders component
     render() {
         return (
             <div>
@@ -11,7 +13,9 @@ export class Dashboard extends React.Component {
         );
     }
 
+
     componentDidMount() {
+        // checking login status
         let xhr = checkAPICredentials();
         xhr.addEventListener('load', () => JSON.parse(xhr.responseText).status ? console.log('login verified') : this.props.history.push('/login'));
     }

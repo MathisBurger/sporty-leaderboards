313f4143c9774b690dc96871a7452cdfc59e2504
Mathis <handymathis@gmail.com>
2021-03-14 19:20:22 +0100

    updated XHR.js

--- frontend-web/src/utils/XHR.js
+++ frontend-web/src/utils/XHR.js
@@ -2,7 +2,8 @@
 // returns XMLHttp object
 export function getXHRConnection(method, path, data, contentType) {
     var xhr = new XMLHttpRequest();
-    xhr.open(method, 'http://127.0.0.1:8080/api' + path);
+    let full = window.location.protocol + '//' + window.location.hostname + ':' + window.location.port + '/api';
+    xhr.open(method, full + path);
     xhr.setRequestHeader('Content-Type', contentType);
     xhr.setRequestHeader('Accept', 'application/json');
     xhr.send(data);
